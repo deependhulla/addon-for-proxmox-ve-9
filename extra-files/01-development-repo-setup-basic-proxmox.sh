581cacc62ab4d89ad252f89f4100db0a7e43c389
Deepen Dhulla <deepen@deependhulla.com>
2026-09-05 12:33:58 +0530

    update ceph repo

--- extra-files/01-development-repo-setup-basic-proxmox.sh
+++ extra-files/01-development-repo-setup-basic-proxmox.sh
@@ -2,6 +2,15 @@
 
 
 echo "" > /etc/apt/sources.list.d/pve-enterprise.sources
+echo "" > /etc/apt/sources.list.d/ceph.sources
+
+
+echo "Types: deb" >> /etc/apt/sources.list.d/ceph.sources
+echo "URIs: http://download.proxmox.com/debian/ceph-tentacle" >> /etc/apt/sources.list.d/ceph.sources
+echo "Suites: trixie" >> /etc/apt/sources.list.d/ceph.sources
+echo "Components: pve-no-subscription" >> /etc/apt/sources.list.d/ceph.sources
+echo "Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg" >> /etc/apt/sources.list.d/ceph.sources
+
 
 echo "Types: deb" >> /etc/apt/sources.list.d/pve-enterprise.sources
 echo "URIs: http://download.proxmox.com/debian/pve" >> /etc/apt/sources.list.d/pve-enterprise.sources
